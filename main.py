df07a8f61cfbb04fdd47d08aa3f3e76a110123cd
sos-mislom <60609474+sos-mislom@users.noreply.github.com>
2020-11-25 22:14:07 +0500

    sdsdasda

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
-import random
+from random import randint
 from PyQt5.QtGui import QBrush, QPainter, QColor, QPalette, QFont
 from PyQt5.QtCore import Qt, QRect, QMetaObject, QCoreApplication
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
-class MyWidget(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setGeometry(QRect(10, 300, 381, 51))
+        font = QFont()
+        font.setPointSize(19)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Yellow circles"))
+        self.pushButton.setText(_translate("Form", "батон"))
+
+
+class YellowCircle(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
-        self.num = random.randint(10, 150)
+        self.setGeometry(300, 300, 400, 400)
+        brush = QBrush(QColor(100, 100, 100))
+        self.r = randint(10, 100)
+        self.color1 = randint(0, 255)
+        self.color2 = randint(0, 255)
+        self.color3 = randint(0, 255)
+
         self.show()
 
     def run(self):
-        self.num = random.randint(10, 150)
+        self.r = randint(10, 100)
+        self.color1 = randint(0, 255)
+        self.color2 = randint(0, 255)
+        self.color3 = randint(0, 255)
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        self.draw(qp)
+        self.drawCircle(qp)
         qp.end()
 
-    def draw(self, qp):
-        qp.setBrush(QColor('Yellow'))
-        qp.drawEllipse(100 + self.num, 100 + self.num, self.num, self.num)
+    def drawCircle(self, qp):
+        qp.setBrush(QColor(self.color1, self.color2, self.color3))
+        r = self.r
+        qp.drawEllipse(100 - r, 100 - r, r, r)
         self.update()
 
 
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = YellowCircle()
     ex.show()
     sys.exit(app.exec_())
